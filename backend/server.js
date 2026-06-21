05bc8d776ca34ba260e85776c37b00ee37d5f2e3
kekelolyt-glitch <kekelolyt@gmail.com>
2026-06-21 21:10:37 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-// Basit mock Discord API (stabil versiyon)
 app.get("/api/discord", (req, res) => {
 const id = req.query.id;
 
@@ -11,7 +10,6 @@ if (!id) {
 return res.json({ error: "no id provided" });
 }
 
-// eski stabil mock sistem
 res.json({
 id: id,
 username: "demo_user",
